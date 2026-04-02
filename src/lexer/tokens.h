2b56791eb3f45d99bcef4a88c23d82d665e88c7d
samuelncaetano <samueln.caetano42@gmail.com>
2026-04-02 10:49:22 -0300

    feat: adiciona suporte a FLOATNUM e CHAR_LITERAL no lexer

--- src/lexer/tokens.h
+++ src/lexer/tokens.h
@@ -4,6 +4,17 @@
 enum TokenType
 {
     NUM = 256,
+    CHAR_LITERAL
 };
+
+typedef union
+{
+    int intval;
+    float floatval;
+    char charval;
+    char *str;
+} YYSTYPE;
+
+extern YYSTYPE yylval;
 
 #endif
